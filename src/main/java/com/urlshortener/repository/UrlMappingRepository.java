db1799758cc286c9decc2174d3a3cfe3cc6e4b33
Sushant Sharma <backedbypower2424@gmail.com>
2026-07-05 21:03:38 +0530

    feat: Implement authenticated URL shortening functionality

---
+++ src/main/java/com/urlshortener/repository/UrlMappingRepository.java
@@ -0,0 +1,14 @@
+package com.urlshortener.repository;
+
+import com.urlshortener.models.UrlMapping;
+import com.urlshortener.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+    UrlMapping findByShortUrl(String shortUrl);
+    List<UrlMapping> findByUser(User user);
+}
